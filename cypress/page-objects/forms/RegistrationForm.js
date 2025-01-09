2ed213518718d1bec776b2b50f989a2b4567525c
Dar1aH <daxundaintravel@gmail.com>
2025-01-09 22:28:07 +0000

    Cypress assertions, actions and custom commands

---
+++ cypress/page-objects/forms/RegistrationForm.js
@@ -0,0 +1,106 @@
+class RegistrationForm {
+    elements = {
+        nameInputField: () => cy.get('#signupName'),
+        lastNameInputField: () => cy.get('#signupLastName'),
+        emailInputField: () => cy.get('#signupEmail'),
+        passwordInputField: () => cy.get('#signupPassword'),
+        reEnterPasswordField: () => cy.get('#signupRepeatPassword'),
+        registerBtn: () => cy.get('[type="button"].btn-primary')
+    }
+
+    verifyEmptyNameErrorByText(text) {
+        this.elements.nameInputField().focus().blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyEmptyLastNameErrorByText(text) {
+        this.elements.lastNameInputField().focus().blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyEmptyEmailErrorByText(text) {
+        this.elements.emailInputField().focus().blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyEmptyPasswordErrorByText(text) {
+        this.elements.passwordInputField().focus().blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyEmptyReEnterPasswordErrorByText(text) {
+        this.elements.reEnterPasswordField().focus().blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyNameWrongLengthErrorByText(text) {
+        this.elements.nameInputField().type('A', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+
+    }
+
+    verifyNameWrongDataErrorByText(text) {
+        this.elements.nameInputField().type('一只猫', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+
+    }
+
+    verifyLastNameWrongLengthErrorByText(text) {
+        this.elements.lastNameInputField().type('A', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+
+    }
+
+    verifyLastNameWrongDataErrorByText(text) {
+        this.elements.lastNameInputField().type('一只猫', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+
+    }
+
+    verifyInvalidEmailByText(text) {
+        this.elements.emailInputField().type('abcds', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+
+    }
+
+    verifyWrongDataPasswordByText(text) {
+        this.elements.passwordInputField().type('abc', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    verifyPasswordMismatchByText(text) {
+        this.elements.passwordInputField().type('9Fasdfggg', { sensitive: true })
+        this.elements.reEnterPasswordField().type('8Fasdfggg', { sensitive: true }).blur()
+        cy.contains('.invalid-feedback p', text).should('be.visible');
+    }
+
+    enterName() {
+        this.elements.nameInputField().type('Daria', { sensitive: true })
+    }
+
+    enterLastName() {
+        this.elements.lastNameInputField().type('Herasymenko', { sensitive: true })
+    }
+
+    enterEmail() {
+        const uniqueEmail = `testuser+${Date.now()}@example.com`
+        this.elements.emailInputField().type(uniqueEmail)
+    }
+
+    enterPassword() {
+        this.elements.passwordInputField().type('9Fasdfggg', { sensitive: true })
+    }
+
+    reEnterPassword() {
+        this.elements.reEnterPasswordField().type('9Fasdfggg', { sensitive: true })
+    }
+
+    verifyRegisterBtnDisabled() {
+        this.elements.registerBtn().should('be.disabled')
+    }
+    clickRegisterBtn() {
+        this.elements.registerBtn().click()
+    }
+
+}
+export default RegistrationForm
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -30,4 +30,20 @@ Cypress.Commands.add('login', (username, password) => {
     cy.get('.modal-footer .btn-primary').click();
   });
 
+  Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+    if (options && options.sensitive) {
+  
+      options.log = false;
+ 
+      Cypress.log({
+        $el: element,
+        name: 'type',
+        message: '*'.repeat(text.length), 
+      });
+    }
+  
+    return originalFn(element, text, options);
+  });
+  
+
   
